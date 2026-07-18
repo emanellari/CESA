688fa0d53b4092ab8a8278022d5f5959b7ad3f10
emanellari <ermelinda.manellari@fshnstudent.info>
2026-07-18 22:07:18 +0200

    edit backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 from .db import Base, engine
-from .routers.auth_routes import router as auth_router
 from .routers import auth_routes, dataset_routes, stats_routes
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Data Entry API")
-
-app.include_router(auth_router)
 app.include_router(auth_routes.router)
 app.include_router(dataset_routes.router)
 app.include_router(stats_routes.router)
@@ -19,10 +16,3 @@ def root():
 @app.get("/health")
 def health():
     return {"ok": True}
-
-@app.get("/users")
-def get_users():
-    return [
-        {"id": 1, "name": "Ana"},
-        {"id": 2, "name": "Luis"}
-    ]
